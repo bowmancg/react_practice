40b84cae52d7307ffa8c49930df3dd5ba1333306
Chandler Bowman <doctorgrant99@gmail.com>
2023-05-24 19:15:03 -0600

    first commit

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Navbar } from "./src/components/Navbar";
+
+export function App() {
+    return (
+        <div className="App" id="app">
+            <header>
+            <Navbar/>
+            </header>
+            <main>
+
+            </main>
+        </div>
+    )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Navbar() {
+    return (
+        <nav className="bg-primary navbar-expand-lg">
+            <h1>Text</h1>
+        </nav>
+    )
+}
