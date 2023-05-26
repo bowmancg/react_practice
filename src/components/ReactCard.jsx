b9638b0f7281657238a241525225d4c9b2b13d93
Chandler Bowman <doctorgrant99@gmail.com>
2023-05-26 17:51:56 -0600

    changed text color

--- src/components/ReactCard.jsx
+++ src/components/ReactCard.jsx
@@ -3,8 +3,8 @@ import ReactDate from "./ReactDate";
 
 function ReactCard(props) {
     return (
-        <div className="ReactCard mb-3" id="reactcard">
-            <div className="card">
+        <div className="mb-3" id="reactcard">
+            <div className="card text-dark">
             <ReactDate date={props.date} />
             <h3>{props.title}</h3>
             </div>
