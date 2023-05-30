2db01a34d0fb46a3afc58483d7e1a718273d98f6
Chandler Bowman <doctorgrant99@gmail.com>
2023-05-30 17:52:52 -0600

    added button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,9 @@ import React from "react";
 function Navbar() {
     return (
         <div className="Navbar" id="navbar">
-            <nav className="navbar navbar-expand-lg bg-primary">
+            <nav className="navbar navbar-expand-lg p-3 bg-primary">
                 <h1>React Practice</h1>
             </nav>
-            
         </div>
     )
 }
--- src/components/ReactCard.jsx
+++ src/components/ReactCard.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDate from "./ReactDate";
 
 function ReactCard(props) {
+    const [title, setTitle] = useState(props.title)
+    const clickHandler = () => {
+        setTitle('Updated!')
+        console.log(title)
+    }
     return (
         <div className="mb-3" id="reactcard">
             <div className="card text-dark">
-            <ReactDate date={props.date} />
             <h3>{props.title}</h3>
+            <button className="rounded-pill bg-secondary" onClick={clickHandler}>Click</button>
+            <ReactDate date={props.date} />
             </div>
         </div>
     )
